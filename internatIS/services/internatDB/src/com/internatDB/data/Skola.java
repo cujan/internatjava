e55d471cb427d886b82ff11b4cac5d9a0542d540
holubjan <holubjan@738e4894-b5de-b5c9-8d48-af6b609c65ec>
2012-02-07 21:16:31 +0000

    zacaty novy projekt

---
+++ internatIS/services/internatDB/src/com/internatDB/data/Skola.java
@@ -0,0 +1,41 @@
+
+package com.internatDB.data;
+
+
+
+/**
+ *  internatDB.Skola
+ *  02/07/2012 22:04:26
+ * 
+ */
+public class Skola {
+
+    private Integer id;
+    private String nazov;
+    private String adresa;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNazov() {
+        return nazov;
+    }
+
+    public void setNazov(String nazov) {
+        this.nazov = nazov;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+}
---
+++ internatIS/webapproot/internatIS.js
@@ -0,0 +1,21 @@
+dojo.declare("internatIS", wm.Application, {
+	"dialogAnimationTime": 350, 
+	"disableDirtyEditorTracking": false, 
+	"i18n": false, 
+	"main": "Main", 
+	"projectSubVersion": "Alpha", 
+	"projectVersion": 1, 
+	"saveCounter": 6, 
+	"studioVersion": "6.4.4GA", 
+	"theme": "wm_harrypotter", 
+	"toastPosition": "br",
+	"widgets": {
+		silkIconList: ["wm.ImageList", {"colCount":39,"height":16,"iconCount":90,"url":"lib/images/silkIcons/silk.png","width":16}, {}]
+	},
+	_end: 0
+});
+
+internatIS.extend({
+
+	_end: 0
+});

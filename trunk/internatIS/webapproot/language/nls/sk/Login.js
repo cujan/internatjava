3c2d9d1ba79e186a251a5bfa2311315c51d3f652
holubjan <holubjan@738e4894-b5de-b5c9-8d48-af6b609c65ec>
2012-02-12 20:51:06 +0000

    pridane vrstvy, upravene a nalinkovane menu

---
+++ trunk/internatIS/webapproot/language/nls/sk/Login.js
@@ -0,0 +1,53 @@
+{
+	"layoutBox": {
+		"_classes": {
+			"domNode": []
+		}
+	},
+	"loginButton": {
+		"_classes": {
+			"domNode": []
+		},
+		"caption": "Prihlásenie",
+		"width": "110px"
+	},
+	"loginButtonPanel": {
+		"_classes": {
+			"domNode": []
+		}
+	},
+	"loginErrorMsg": {
+		"_classes": {
+			"domNode": []
+		}
+	},
+	"loginInputPanel": {
+		"_classes": {
+			"domNode": []
+		}
+	},
+	"loginMainPanel": {
+		"_classes": {
+			"domNode": []
+		}
+	},
+	"passwordInput": {
+		"_classes": {
+			"domNode": []
+		},
+		"border": null,
+		"captionSize": "130px"
+	},
+	"usernameInput": {
+		"_classes": {
+			"domNode": []
+		},
+		"captionSize": "130px",
+		"width": "310px"
+	},
+	"wmTitle": {
+		"_classes": {
+			"domNode": []
+		}
+	}
+}
